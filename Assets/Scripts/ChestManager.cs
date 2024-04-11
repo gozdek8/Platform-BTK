99d42d9f1038831f5972289d36941404254ced87
Gözde <gkahraman03@gmail.com>
2024-04-11 18:52:35 +0300

    Coin number error fixed

--- Assets/Scripts/ChestManager.cs
+++ Assets/Scripts/ChestManager.cs
@@ -24,7 +24,7 @@ public class ChestManager : MonoBehaviour
 
     void OpenChest()
     {
-        if (DataManager.Instance.CoinsCollected == 45)
+        if (DataManager.Instance.CoinsCollected == 43)
         {
             GetComponent<Animator>().SetBool("isEnough",true);
             audioClip.Play();
